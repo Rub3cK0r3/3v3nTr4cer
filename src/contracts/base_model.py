3cefa313e5d3d96b2cc2d70390d87e3ee30cacce
Rub3ck0r3 <rubecode11001@gmail.com>
2026-08-26 11:40:06 +0200

    fix/chore: Updated contracts comments for better explanation and fix handle worker exceptions without disrupting the loop

--- src/contracts/base_model.py
+++ src/contracts/base_model.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import DeclarativeBase
 
-# This is the base_model we use in the rest of contract models implementation
-############################################################################
-
+# This is the base implementation for all the models in the contracts module, it is used to create the tables in the database
+# This is pretty common practice in SQLAlchemy to have a base class that all models inherit from, so 
+# that they can share common functionality and metadata.
+# As I come from Django and Basic Java this would be similar to having an abstract base class 
+# that all models inherit from, so that they can share common functionality and metadata.
 class Base(DeclarativeBase):
     pass
